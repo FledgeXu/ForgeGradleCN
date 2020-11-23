3f2ef88f75d97f8c08c0719370f83138c1d7b909
FledgeXu <xzk0701@gmail.com>
2020-11-23 15:05:54 +0800

    Adjust Mirrors and Add some info.

--- src/common/java/net/minecraftforge/gradle/common/util/Mirrors.java
+++ src/common/java/net/minecraftforge/gradle/common/util/Mirrors.java
@@ -32,9 +32,9 @@ public class Mirrors {
     public static final String ALIYUN_CENTRAL = "https://maven.aliyun.com/repository/public";
 
     public static void changeMirror(Project project) {
-        MINECRAFT_JAR = project.hasProperty("MINECRAFT_JAR") ? ((String) Objects.requireNonNull(project.property("MINECRAFT_JAR"))).replaceAll("/$", "") : "https://download.mcbbs.net";
+        MINECRAFT_JAR = project.hasProperty("MINECRAFT_JAR") ? ((String) Objects.requireNonNull(project.property("MINECRAFT_JAR"))).replaceAll("/$", "") : "https://mc.mirrors.tmysam.top";
         FORGE_MAVEN = project.hasProperty("FORGE_MAVEN") ? ((String) Objects.requireNonNull(project.property("FORGE_MAVEN"))).replaceAll("/$", "") : "https://lss233.littleservice.cn/repositories/forgedev";
-        MINECRAFT_LIB = project.hasProperty("MINECRAFT_LIB") ? ((String) Objects.requireNonNull(project.property("MINECRAFT_LIB"))).replaceAll("/$", "") : "https://download.mcbbs.net/maven";
-        MINECRAFT_ASSETS = project.hasProperty("MINECRAFT_ASSETS") ? ((String) Objects.requireNonNull(project.property("MINECRAFT_ASSETS"))).replaceAll("/$", "") : "https://download.mcbbs.net/assets";
+        MINECRAFT_LIB = project.hasProperty("MINECRAFT_LIB") ? ((String) Objects.requireNonNull(project.property("MINECRAFT_LIB"))).replaceAll("/$", "") : "https://mclib.mirrors.tmysam.top";
+        MINECRAFT_ASSETS = project.hasProperty("MINECRAFT_ASSETS") ? ((String) Objects.requireNonNull(project.property("MINECRAFT_ASSETS"))).replaceAll("/$", "") : "https://mcres.mirrors.tmysam.top";
     }
 }
--- src/fgcn/java/com/otakusaikou/fgcn/userdev/UserDevPluginWrapper.java
+++ src/fgcn/java/com/otakusaikou/fgcn/userdev/UserDevPluginWrapper.java
@@ -1,13 +1,23 @@
 package com.otakusaikou.fgcn.userdev;
 
+import net.minecraftforge.gradle.common.util.Mirrors;
 import net.minecraftforge.gradle.userdev.UserDevPlugin;
 import org.gradle.api.Project;
+import org.gradle.api.logging.Logger;
 
 import javax.annotation.Nonnull;
 
 public class UserDevPluginWrapper extends UserDevPlugin {
     @Override
     public void apply(@Nonnull Project project) {
+        final Logger logger = project.getLogger();
+        logger.lifecycle("You are using the ForgeGradleCN which isn't supported by Forge Team");
+        logger.lifecycle("你正在使用 ForgeGradleCN，它不受到 Forge 团队支持。");
+        Mirrors.changeMirror(project);
+        logger.lifecycle("MINECRAFT_JAR 版本: " + Mirrors.MINECRAFT_JAR);
+        logger.lifecycle("FORGE_MAVEN 版本: " + Mirrors.MINECRAFT_JAR);
+        logger.lifecycle("MINECRAFT_LIB 版本: " + Mirrors.MINECRAFT_JAR);
+        logger.lifecycle("MINECRAFT_ASSETS 版本: " + Mirrors.MINECRAFT_JAR);
         super.apply(project);
     }
 }
