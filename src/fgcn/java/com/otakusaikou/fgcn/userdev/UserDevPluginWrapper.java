71cc536433f4dc1040a7214178355c7cdcb7d135
FledgeXu <xzk0701@gmail.com>
2020-11-24 09:23:16 +0800

    Fix bugs and add UA.

--- src/fgcn/java/com/otakusaikou/fgcn/userdev/UserDevPluginWrapper.java
+++ src/fgcn/java/com/otakusaikou/fgcn/userdev/UserDevPluginWrapper.java
@@ -15,9 +15,10 @@ public class UserDevPluginWrapper extends UserDevPlugin {
         logger.lifecycle("你正在使用 ForgeGradleCN，它不受到 Forge 团队支持。");
         Mirrors.changeMirror(project);
         logger.lifecycle("MINECRAFT_JAR 版本: " + Mirrors.MINECRAFT_JAR);
-        logger.lifecycle("FORGE_MAVEN 版本: " + Mirrors.MINECRAFT_JAR);
-        logger.lifecycle("MINECRAFT_LIB 版本: " + Mirrors.MINECRAFT_JAR);
-        logger.lifecycle("MINECRAFT_ASSETS 版本: " + Mirrors.MINECRAFT_JAR);
+        logger.lifecycle("FORGE_MAVEN 版本: " + Mirrors.FORGE_MAVEN);
+        logger.lifecycle("MINECRAFT_LIB 版本: " + Mirrors.MINECRAFT_LIB);
+        logger.lifecycle("MINECRAFT_ASSETS 版本: " + Mirrors.MINECRAFT_ASSETS);
+        System.setProperty("http.agent", "ForgeGradleCN/1.0 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36");
         super.apply(project);
     }
 }
